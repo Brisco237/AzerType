bc9f3119f32f28a8962b89580543a495f4ff20b2
kamdem Brice <kamdembrice770@gmail.com>
2025-09-06 03:05:56 +0100

    jeu GUESS the word

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let compteur = 0  
-while(compteur < 11){
-    console.log(compteur)
-    compteur=compteur + 1
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !","La vie,l'univers et le reste", "Merci pour le poisson"]
+let score = 0
+
+let choix = prompt("Voulez-vous des mots ou des phrases ? (mots/phrases)")
+while(choix !== "mots" && choix !== "phrases"){
+    choix = prompt("Voulez-vous des mots ou des phrases ? (mots/phrases)")
 }
+
+if(choix === "mots"){
+    for(let i = 0; i < listeMots.length; i++){
+        let reponseUser = prompt("Entrez un mot :")
+        if(reponseUser === listeMots[i]){
+            console.log("bravo")
+            score += 1
+        }
+    }
+    console.log("votre score est de " + score + "/" + listeMots.length)
+}else{
+    for (let i = 0; i < listePhrases.length; i++) {
+        let reponseUser = prompt("Entrez une phrase :")
+        if (reponseUser === listePhrases[i]) {
+            console.log("bravo")
+            score += 1
+        }
+    }
+    console.log("votre score est de " + score + "/" + listePhrases.length)
+}
+
