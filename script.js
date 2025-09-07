60ec972527635cc50bf7522f2a6c511674471d72
kamdem Brice <kamdembrice770@gmail.com>
2025-09-07 14:37:50 +0100

    commenter tout le code pour recoder moi meme

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let btnValiderMot = document.getElementById("btnValiderMot")
+let inputEcriture = document.getElementById("inputEcriture")
 
+/*
 function afficherProposition(proposition){
     let divProposition = document.querySelector(".zoneProposition")
     divProposition.innerText = proposition
 }
-
 function lancerJeu(){
     let score = 0
     let nombreDeMots = 0
-
-    let divProposition = document.querySelector(".zoneProposition")
-    let btnValiderMot = document.getElementById("btnValiderMot")
-    let inputEcriture = document.getElementById("inputEcriture")
     let i = 0
+
     afficherProposition(listeMots[i])
     btnValiderMot.addEventListener("click", () => {
-        afficherProposition(listeMots[i])
+        if(inputEcriture.value === listeMots[i]){
+            score++
+        }
         i++
         inputEcriture.value = ""
         if(listeMots[i] === undefined){
@@ -24,6 +25,7 @@ function lancerJeu(){
         }
     })
 }
-
 lancerJeu()
+*/  
+
 
