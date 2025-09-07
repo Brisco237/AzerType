502c6443c6fd28b28939b0ebe0166f7b1a0bbf02
kamdem Brice <kamdembrice770@gmail.com>
2025-09-07 02:20:38 +0100

    Ajouter les mots de la liste a la page html

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 
+function afficherProposition(proposition){
+    let divProposition = document.querySelector(".zoneProposition")
+    divProposition.innerText = proposition
+}
+
 function lancerJeu(){
     let score = 0
     let nombreDeMots = 0
 
+    let divProposition = document.querySelector(".zoneProposition")
     let btnValiderMot = document.getElementById("btnValiderMot")
     let inputEcriture = document.getElementById("inputEcriture")
     let i = 0
+    afficherProposition(listeMots[i])
     btnValiderMot.addEventListener("click", () => {
-        console.log(inputEcriture.value)
+        afficherProposition(listeMots[i])
         i++
+        inputEcriture.value = ""
+        if(listeMots[i] === undefined){
+            afficherProposition("Jeu terminé !")
+            btnValiderMot.disabled = true
+        }
     })
-
-    afficherResultat(score, nombreDeMots)
 }
 
 lancerJeu()
