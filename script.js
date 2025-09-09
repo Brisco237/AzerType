2bed77da4d3729e186f670605b0be69735c35a7c
kamdem Brice <kamdembrice770@gmail.com>
2025-09-09 02:32:08 +0100

    add outline:none in boutton validermot

--- script.js
+++ script.js
@@ -7,15 +7,13 @@ let zoneScore = document.querySelector(".zoneScore span")
 let zoneProposition = document.querySelector(".zoneProposition")
 
 // Essayez d'afficher les élements récupérés
-zoneProposition.innerHTML = listeMots[0]
 let i = 0
 let score = 0
+zoneProposition.innerHTML = listeMots[0]
 btnValiderMot.addEventListener("click", ()=>{
-    console.log(inputEcriture.value)
     if(inputEcriture.value === listeMots[i]){
         score++
     }
-    i++
-    zoneScore.innerHTML = score + " / "+ listeMots.length
+    zoneScore.innerHTML = score + " / " + listeMots.length
     inputEcriture.value = ""
 })
